faaa2975b46912a87fa0faef8f65b0e954f4087f
Oleh <kudolel@hotmail.com>
2019-09-11 09:57:56 +0200

    Removed not necessary code

--- ExampleMVVM/Data/PersistentStorages/UserDefaultsStorage/DataMapping/MovieQueryUDSMapping.swift
+++ ExampleMVVM/Data/PersistentStorages/UserDefaultsStorage/DataMapping/MovieQueryUDSMapping.swift
@@ -15,20 +15,6 @@ struct MovieQueriesListUDS: Codable {
 struct MovieQueryUDS: Codable {
 
     let query: String
-    
-    private enum CodingKeys: String, CodingKey {
-        case query
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.query = try container.decode(String.self, forKey: .query)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(query, forKey: .query)
-    }
 }
 
 extension MovieQueryUDS {
