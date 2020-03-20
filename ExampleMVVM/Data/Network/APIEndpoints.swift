5b5c6ccb0e5e967f4c6fe6e96023ff64ae04f42c
Oleh Kudinov <kudoleh@hotmail.com>
2020-03-20 22:59:21 +0000

    Minor refactor

--- ExampleMVVM/Data/Network/APIEndpoints.swift
+++ ExampleMVVM/Data/Network/APIEndpoints.swift
@@ -22,10 +22,8 @@ struct APIEndpoints {
         let sizes = [92, 185, 500, 780]
         let closestWidth = sizes.enumerated().min { abs($0.1 - width) < abs($1.1 - width) }?.element ?? sizes.first!
         
-        return Endpoint(
-            path: "t/p/w\(closestWidth)\(path)",
-            method: .get,
-            responseDecoder: RawDataResponseDecoder()
-        )
+        return Endpoint(path: "t/p/w\(closestWidth)\(path)",
+                        method: .get,
+                        responseDecoder: RawDataResponseDecoder())
     }
 }
