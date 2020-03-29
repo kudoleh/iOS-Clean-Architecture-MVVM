1f088234d6cbeca64574a1b6e59d0cc9d8e432ec
Oleh Kudinov <kudoleh@hotmail.com>
2020-03-29 21:58:52 +0100

    Remove space

--- ExampleMVVM/Infrastructure/Network/DataTransferService.swift
+++ ExampleMVVM/Infrastructure/Network/DataTransferService.swift
@@ -20,7 +20,6 @@ public protocol DataTransferService {
     @discardableResult
     func request<T: Decodable, E: ResponseRequestable>(with endpoint: E,
                                                        completion: @escaping CompletionHandler<T>) -> NetworkCancellable? where E.Response == T
-
     @discardableResult
     func request<E: ResponseRequestable>(with endpoint: E,
                                          completion: @escaping CompletionHandler<Void>) -> NetworkCancellable? where E.Response == Void
