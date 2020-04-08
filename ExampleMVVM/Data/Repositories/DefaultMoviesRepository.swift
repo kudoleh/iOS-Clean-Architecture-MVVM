ef8f93f1eb00997960f4ee9dbd5964da8d0f7892
Oleh Kudinov <kudoleh@hotmail.com>
2020-04-08 22:36:37 +0100

    Revert change

--- ExampleMVVM/Data/Repositories/DefaultMoviesRepository.swift
+++ ExampleMVVM/Data/Repositories/DefaultMoviesRepository.swift
@@ -36,7 +36,13 @@ extension DefaultMoviesRepository: MoviesRepository {
 
             let endpoint = APIEndpoints.getMovies(with: requestDTO)
             task.networkTask = self.dataTransferService.request(with: endpoint) { result in
-                completion(result.map { $0.mapToDomain() })
+                switch result {
+                case .success(let responseDTO):
+                    self.cache.save(response: responseDTO, for: requestDTO)
+                    completion(.success(responseDTO.mapToDomain()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
             }
         }
         return task
