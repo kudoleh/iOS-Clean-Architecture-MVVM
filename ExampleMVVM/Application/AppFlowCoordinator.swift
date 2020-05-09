43b04c93c0372ce4c7348f6b6d949e898719522f
Oleh Kudinov <kudoleh@hotmail.com>
2020-05-09 21:15:39 +0100

    Update comment

--- ExampleMVVM/Application/AppFlowCoordinator.swift
+++ ExampleMVVM/Application/AppFlowCoordinator.swift
@@ -19,7 +19,7 @@ class AppFlowCoordinator {
     }
 
     func start() {
-        // In App Flow we can check if user needs to login, if yes we would run login flow.
+        // In App Flow we can check if user needs to login, if yes we would run login flow
         let moviesSceneDIContainer = appDIContainer.makeMoviesSceneDIContainer()
         let flow = moviesSceneDIContainer.makeMoviesSearchFlowCoordinator(navigationController: navigationController)
         flow.start()
