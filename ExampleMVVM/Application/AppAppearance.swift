0fc1d78b30a6467a899b2878fbd0d66072074048
Oleh Kudinov <kudoleh@hotmail.com>
2022-02-02 21:31:42 +0100

    update navigation bar colors for ios 15

--- ExampleMVVM/Application/AppAppearance.swift
+++ ExampleMVVM/Application/AppAppearance.swift
@@ -11,9 +11,18 @@ import UIKit
 final class AppAppearance {
     
     static func setupAppearance() {
-        UINavigationBar.appearance().barTintColor = .black
-        UINavigationBar.appearance().tintColor = .white
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            appearance.backgroundColor = UIColor(red: 37/255.0, green: 37/255.0, blue: 37.0/255.0, alpha: 1.0)
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        } else {
+            UINavigationBar.appearance().barTintColor = .black
+            UINavigationBar.appearance().tintColor = .white
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        }
     }
 }
 
