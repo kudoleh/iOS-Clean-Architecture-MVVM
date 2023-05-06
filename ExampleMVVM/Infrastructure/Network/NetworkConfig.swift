e5879c3e1b3ddad5ae0ecf70394775ffa332bfaf
Oleh Kudinov <kudoleh@hotmail.com>
2023-05-06 11:17:09 +0200

    Adjust to code style

--- ExampleMVVM/Infrastructure/Network/NetworkConfig.swift
+++ ExampleMVVM/Infrastructure/Network/NetworkConfig.swift
@@ -11,9 +11,11 @@ struct ApiDataNetworkConfig: NetworkConfigurable {
     let headers: [String: String]
     let queryParameters: [String: String]
     
-     init(baseURL: URL,
-                 headers: [String: String] = [:],
-                 queryParameters: [String: String] = [:]) {
+     init(
+        baseURL: URL,
+        headers: [String: String] = [:],
+        queryParameters: [String: String] = [:]
+     ) {
         self.baseURL = baseURL
         self.headers = headers
         self.queryParameters = queryParameters
